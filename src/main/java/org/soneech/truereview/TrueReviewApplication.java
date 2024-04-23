00aaba2fbe9da1683ef8a9e5f289a56df22f38da
Soneech <sonya.patrina@yandex.ru>
2024-04-23 14:42:27 +0300

    Initial commit

---
+++ src/main/java/org/soneech/truereview/TrueReviewApplication.java
@@ -0,0 +1,12 @@
+package org.soneech.truereview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrueReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrueReviewApplication.class, args);
+    }
+}
